e90ad8d3d129bd1aa44be196752fc58a1c38de72
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-10-04 21:44:20 +0100

    New: Use AudioOutputStream instead of nullOutputStream

--- src/main/kotlin/dev/cbyrne/adieux/impl/spotify/mixing/AdieuxSinkOutput.kt
+++ src/main/kotlin/dev/cbyrne/adieux/impl/spotify/mixing/AdieuxSinkOutput.kt
@@ -4,7 +4,11 @@ import xyz.gianlu.librespot.player.mixing.output.OutputAudioFormat
 import xyz.gianlu.librespot.player.mixing.output.SinkOutput
 import java.io.OutputStream
 
-class AdieuxSinkOutput(private val stream: OutputStream = OutputStream.nullOutputStream()) : SinkOutput {
+class AdieuxSinkOutput : SinkOutput {
+    companion object {
+        val stream = AudioOutputStream()
+    }
+
     override fun write(buffer: ByteArray, offset: Int, len: Int) = stream.write(buffer, offset, len)
     override fun close() = stop()
 
@@ -15,4 +19,43 @@ class AdieuxSinkOutput(private val stream: OutputStream = OutputStream.nullOutpu
     override fun drain() = stream.flush()
     override fun flush() = stream.flush()
     override fun stop() = stream.close()
+
+    /**
+     * An [OutputStream] class which converts the received bytes from little endian to big endian.
+     * We do this because Discord requires big endian, not little endian.
+     */
+    class AudioOutputStream : OutputStream() {
+        val bytes = mutableListOf<Int>()
+
+        override fun write(b: Int) {
+            error("Unsupported")
+        }
+
+        override fun write(b: ByteArray) = write(b, 0, b.size)
+
+        override fun write(array: ByteArray, off: Int, len: Int) {
+            var i = 0
+            while (i < len && i + 4 <= len) {
+                val a = array[i].toInt()
+                val b = array[i + 1].toInt()
+                val c = array[i + 2].toInt()
+                val d = array[i + 3].toInt()
+
+                val int = (a and 0xFF) or (b and 0xFF shl 8) or (c and 0xFF shl 16) or (d and 0xFF shl 24)
+
+                println("Bytes: [$a, $b, $c, $d]")
+                println("Value: $int")
+
+                bytes.add(int)
+
+                i += 4
+            }
+        }
+
+        override fun close() {
+        }
+
+        override fun flush() {
+        }
+    }
 }
